83a43356b5d2cd2e72ef80b5bdd79bd2cdba7d17
Arthur Noel <arthur@0compute.net>
2018-07-17 19:49:47 +0100

    release 0.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="distinfo",
-    version="0.0.0",
+    version="0.1.0",
     author="Arthur Noel",
     author_email="arthur@0compute.net",
     description="Extract metadata from Python source distributions",

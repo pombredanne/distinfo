e69b9770cc636c5fabff9fd1d71c4524a6b15aa4
Arthur Noel <arthur@0compute.net>
2018-07-29 15:13:05 +0100

    improved tox collector

--- distinfo/collectors/toxini.py
+++ distinfo/collectors/toxini.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from pathlib import Path
 import re
 import sys
 
@@ -15,7 +16,15 @@ log = logging.getLogger(__name__)
 
 class ToxIni(Collector):
 
-    ENV_NAME_PATTERN = re.compile("^py[0-9]+")
+    ENV_IGNORE_PATTERN = re.compile("doc|lint")
+
+    def _detox(self, config, expr):
+        cwd = os.getcwd()
+        expr = expr.replace(str(config.envbindir) + "/", "")
+        expr = expr.replace(str(config.envdir) + "/", "")
+        expr = expr.replace(cwd + "/", "")
+        expr = expr.replace(cwd, "")
+        return expr
 
     def _collect(self):
 
@@ -28,24 +37,34 @@ class ToxIni(Collector):
             self._warn_exc(exc)
             return
 
-        name = "py%d%d" % (sys.version_info.major, sys.version_info.minor)
-        configs = sorted([c for c in toxconf.envconfigs.values()
-                          if self.ENV_NAME_PATTERN.match(c.envname)],
-                         key=lambda c: c.envname)
-        for config in configs:
-            if config.envname.startswith(name):
-                break
+        python = "python%d.%d" % (sys.version_info.major, sys.version_info.minor)
+        configs = sorted(
+            filter(
+                lambda c: not self.ENV_IGNORE_PATTERN.match(c.envname),
+                toxconf.envconfigs.values(),
+            ),
+            key=lambda c: c.envname,
+            reverse=True,
+        )
+        match_configs = [c for c in configs
+                         if Path(c.basepython).name in (python, python + "m")]
+        if match_configs:
+            envname = "py%d%d" % (sys.version_info.major, sys.version_info.minor)
+            for config in match_configs:
+                if config.envname == envname:
+                    break
+            else:
+                for config in match_configs:
+                    if config.envname.startswith("py"):
+                        break
+                else:
+                    config = match_configs[0]
         else:
             if not configs:
+                log.warning("%r no config found", self)
                 return
-            # take the last environment which will be the highest python
-            config = configs[-1]
-            log.warning(
-                "%r has no environment for %r, falling back to %r",
-                self,
-                name,
-                config.envname,
-            )
+            config = configs[0]
+            log.warning("%r has no config for %s, using %s", self, python, config.envname)
 
         # dependencies
         for dep in config.deps:
@@ -60,32 +79,17 @@ class ToxIni(Collector):
                     continue
                 self.add_requirements_file(reqs_file, "test")
             else:
-                # don't allow pinned dependencies
+                # unpin dependencies
                 dep = dep.name.split("==")[0].strip()
                 self.add_requirement(dep, "test")
 
         # environment
         env = Munch()
         for key in config.setenv.keys():
-            if key in ("PYTHONHASHSEED", "PYTHONPATH"):
-                continue
-            env[key] = config.setenv[key].replace(os.getcwd(), ".")
+            if key != "PYTHONHASHSEED":
+                env[key] = self._detox(config, config.setenv[key])
 
         # commands
-        cwd = os.getcwd()
-        commands = []
-        for command in config.commands:
-            if command[0] == "pip":
-                log.warning("%r ignoring command %r", self, command)
-            else:
-                cmd = []
-                for expr in command:
-                    expr = expr.replace(str(config.envbindir) + "/", "")
-                    expr = expr.replace(str(config.envdir) + "/", "")
-                    expr = expr.replace(str(config.envname), "result")
-                    expr = expr.replace(cwd + "/", "")
-                    expr = expr.replace(cwd, ".")
-                    cmd.append(expr)
-                commands.append(cmd)
+        commands = [[self._detox(config, c) for c in cmd] for cmd in config.commands]
 
         self.ext.tox = Munch(commands=commands, env=env)
--- tests/unit/collector/test_toxini.py
+++ tests/unit/collector/test_toxini.py
@@ -1,3 +1,5 @@
+import sys
+
 from tox.exception import ConfigError
 
 from distinfo.collectors import toxini
@@ -7,7 +9,6 @@ from .cases import Case
 TOXINI = """
 [tox]
 envlist = py27, pypy, py34, py35, py36, py37
-
 [testenv]
 deps =
     zzz
@@ -15,9 +16,8 @@ deps =
     -r requirements-missing.txt
     -c xxx
 commands =
-    pip install xxx
-    python -m pytest
-    - false
+    tick
+    tock
 setenv =
     ONE = 1
 """
@@ -28,9 +28,18 @@ REQUIREMENTS = """
 aaa
 """
 
+TOXINI_ALT = """
+[tox]
+envlist = xxx
+[xxx]
+basepython = python%d.%d
+""" % (sys.version_info.major, sys.version_info.minor)
+
 TOXINI_BAD = """
 [tox]
 envlist = xxx
+[testenv]
+basepython = xxx
 """
 
 TOXINI_BAD_ALT = """
@@ -38,32 +47,50 @@ TOXINI_BAD_ALT = """
 envlist = py10
 """
 
+TOXINI_NOCONFIG = """
+[tox]
+envlist = doc
+"""
+
 
 class TestRequirementsFile(Case):
 
     collector = toxini.ToxIni
 
-    def test_collect(self, caplog, tmpdir):
+    def test_collect(self, tmpdir):
         tmpdir.join("tox.ini").write(TOXINI)
         tmpdir.join("requirements.txt").write(REQUIREMENTS)
         collector = self._collect(tmpdir)
         assert {"aaa", "zzz"} == collector.requires.test
-        assert [["python", "-m", "pytest"], ["-", "false"]] == collector.ext.tox.commands
+        assert [["tick"], ["tock"]] == collector.ext.tox.commands
         assert collector.ext.tox.env.ONE == "1"
-        assert "ignoring command" in caplog.text
+
+    def _assert_empty(self, collector):
+        assert not collector.ext.tox
+        assert not collector.requires
+
+    def test_collect_alt(self, tmpdir):
+        tmpdir.join("tox.ini").write(TOXINI_ALT)
+        collector = self._collect(tmpdir)
+        self._assert_empty(collector)
+
+    def test_collect_noconfig(self, tmpdir):
+        tmpdir.join("tox.ini").write(TOXINI_NOCONFIG)
+        collector = self._collect(tmpdir)
+        self._assert_empty(collector)
 
     def test_collect_bad(self, tmpdir):
         tmpdir.join("tox.ini").write(TOXINI_BAD)
         collector = self._collect(tmpdir)
-        assert not collector.ext.tox
-        assert not collector.requires
+        self._assert_empty(collector)
+
+    def test_collect_bad_alt(self, tmpdir):
         tmpdir.join("tox.ini").write(TOXINI_BAD_ALT)
         collector = self._collect(tmpdir)
-        assert not collector.ext.tox
-        assert not collector.requires
+        self._assert_empty(collector)
 
     def test_collect_conf_error(self, monkeypatch, tmpdir):
         monkeypatch.setattr(toxini, "parseconfig", self._raiser(ConfigError))
         tmpdir.join("tox.ini").write(TOXINI)
         collector = self._collect(tmpdir)
-        assert not collector.requires
+        self._assert_empty(collector)

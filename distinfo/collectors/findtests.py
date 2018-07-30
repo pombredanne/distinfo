9cb2f0b9f73e9b7e55e46ebe9bd4a5ce4a2f5f36
Arthur Noel <arthur@0compute.net>
2018-07-30 14:46:26 +0100

    do not count *pytest*.py as a test file

--- distinfo/collectors/findtests.py
+++ distinfo/collectors/findtests.py
@@ -4,4 +4,10 @@ from .collector import Collector
 class FindTests(Collector):
 
     def _collect(self):
-        self.ext.tests = set(map(lambda p: str(p.parent), self.path.glob("**/*test*.py")))
+        self.ext.tests = set(map(
+            lambda p: str(p.parent),
+            filter(
+                lambda p: "pytest" not in p.name,
+                self.path.glob("**/*test*.py"),
+            ),
+        ))
